ac16be48d970c30a494bbd7ef6a483ec6160ee62
Aditya Rajput <adityarajput81971@gmail.com>
2025-06-28 11:53:55 +0530

    keyboard added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,82 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { betAmt, togglefooter, togglemain } from '../features/mines/mineSlices'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { FaMinus } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
 import { BiSolidCoinStack } from "react-icons/bi";
 import { CiPlay1 } from "react-icons/ci";
 import { MdOutlineAutorenew } from "react-icons/md";
+import { FiDelete } from "react-icons/fi";
+import { MdOutlineDone } from "react-icons/md";
 
 const Footer = () => {
-    const [betamt, setBetamt] = useState(10)
+    const [betamount, setBetamount] = useState("10")
     const [disablefooter, setDisablefooter] = useState(false)
+    const [keyboard, setKeyboard] = useState(false)
+    const [numkeys, setNumkeys] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0, "delete"])
     const dispatch = useDispatch()
     const footerselector = useSelector(state => state.disablefooter)
-    console.log(footerselector,"hiii")
+    const mainselector = useSelector(state => state.disablemain)
+    console.log(footerselector, "hiii")
 
-    const bet = (e)=>{
-        dispatch(betAmt(betamt))
-        dispatch(togglemain(false))
-        setDisablefooter(true)
-        dispatch(togglefooter(true))
+    const bet = (e) => {
+        if (mainselector&&keyboard==false) {
+            
+            dispatch(betAmt(parseInt(betamount)))
+            dispatch(togglemain(false))
+            setDisablefooter(true)
+            dispatch(togglefooter(true))
+            
+        }
+    }
+    const openkeyboard = () => {
+        setKeyboard(!keyboard)
+         if(betamount>=100&&keyboard==true){
+         setBetamount("100")
+        }
+    }
+    const changevalue = (num)=>{
+       if (num=="delete") {
+        setBetamount(betamount.slice(0,-1))
+       } else {
+       setBetamount(betamount+num.toString())
+       }
     }
     
     useEffect(() => {
-        console.log(footerselector,"heee")
-     setDisablefooter(footerselector)
+        console.log(footerselector, "heee")
+        setDisablefooter(footerselector)
     }, [footerselector])
-    
+
     return (
-        <footer className={`bg-blue-800  w-full md:h-20 h-2/10 rounded-2xl flex md:flex-row flex-col-reverse gap-x-2 gap-y-2 justify-center cursor-pointer items-center ${disablefooter?"disable-div":""}`}>
+        <footer className={`bg-blue-800  w-full md:h-20 h-2/10 rounded-2xl flex md:flex-row flex-col-reverse gap-x-2 gap-y-2 justify-center cursor-pointer items-center ${disablefooter ? "disable-div" : ""}`}>
             <div className='bg-sky-700 md:w-1/3 w-9/10 md:h-7/10 h-1/3 rounded-full flex' >
+                <div className={`h-70 w-60 bg-sky-700 rounded-2xl fixed md:bottom-22 bottom-23 md:left-52  ${keyboard ? "" : "hidden"}`}>
+                    <div className=' m-2 h-9/12 rounded-2xl bg-sky-700 flex flex-wrap gap-x-2 gap-y-2 justify-center items-center'>
+                        {numkeys.map((num, index) => {
+                            return <span onClick={()=>changevalue(num)} className='w-2/7 h-1/5 rounded-xl border-black flex border justify-center items-center text-white bg-sky-700'>{(num=="delete")?<FiDelete />:`${num}`}</span>
+                        })}
+                    </div>
+                    <div onClick={openkeyboard} className=' m-2 h-2/12 bg-sky-900 text-white rounded-2xl flex justify-center items-center'><MdOutlineDone /></div>
+                </div>
                 <div className='flex justify-center gap-y-2 items-center flex-col w-1/2'>
                     <div className='text-white  text-xs font-semibold font-sans'>Bet USD</div>
-                    <input value={betamt} onChange={(e)=> setBetamt(e.target.value)} className='bg-sky-900 border border-black w-8/10 h-6 rounded-full text-white text-center' type="number" name="" id="" />
+                    <div onClick={openkeyboard} className='bg-sky-900 border border-black w-8/10 h-6 rounded-full text-white text-center'>
+                        {betamount}
+                    </div>
                 </div>
                 <div className='w-1/2 h-full flex justify-center items-center gap-x-2'>
-                 <div className='p-2  border border-gray-700 rounded-full text-slate-300'><FaMinus /></div>
-                 <div className='p-2 border border-gray-700 rounded-full text-slate-300 text-2xl'><BiSolidCoinStack /></div>
-                 <div className='p-2 border border-gray-700 rounded-full text-slate-300'><FaPlus /></div>
+                    <div className='p-2  border border-gray-700 rounded-full text-slate-300'><FaMinus /></div>
+                    <div className='p-2 border border-gray-700 rounded-full text-slate-300 text-2xl'><BiSolidCoinStack /></div>
+                    <div className='p-2 border border-gray-700 rounded-full text-slate-300'><FaPlus /></div>
                 </div>
                 <div>
                 </div>
             </div>
-            <div className=' md:w-1/3 w-9/10 md:h-7/10 h-1/3 rounded-full flex  items-center' > 
-            <div className='w-15 mr-5 h-full bg-blue-600 rounded-full flex justify-center items-center text-slate-300 text-3xl'><MdOutlineAutorenew /></div>
-            <div onClick={bet} className='flex  items-center justify-between w-9/10 bg-green-600 h-full rounded-2xl'><CiPlay1 className='text-white font-bold text-3xl ml-5' /><span className='text-white font-bold text-xl w-8/10  mr-10 flex justify-center'>BET</span></div>
+            <div className=' md:w-1/3 w-9/10 md:h-7/10 h-1/3 rounded-full flex  items-center' >
+                <div className='w-15 mr-5 h-full bg-blue-600 rounded-full flex justify-center items-center text-slate-300 text-3xl'><MdOutlineAutorenew /></div>
+                <div onClick={bet} className='flex  items-center justify-between w-9/10 bg-green-600 h-full rounded-2xl'><CiPlay1 className='text-white font-bold text-3xl ml-5' /><span className='text-white font-bold text-xl w-8/10  mr-10 flex justify-center'>BET</span></div>
             </div>
 
         </footer>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,51 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { betAmt, togglefooter,togglemain } from '../features/mines/mineSlices'
+import { betAmt, togglefooter, togglemain } from '../features/mines/mineSlices'
 
 
 
 const Main = () => {
-  const minesCount = 10
+  const [minesCount, setMinesCount] = useState(3)
   const [boxes, setBoxes] = useState([])
   const [revealed, setRevealed] = useState([])
   const [maindisable, setMaindisable] = useState(true)
+const [togglemine, setTogglemine] = useState(true)
+const [totalmines, setTotalmines] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
   const mainselector = useSelector(state => state.disablemain)
+  const footerselector = useSelector(state => state.disablefooter)
   const dispatch = useDispatch()
 
   useEffect(() => {
     resetGame();
-  }, []);
-  useEffect(() => {
-    setMaindisable(mainselector)
-  }, [mainselector])
+  }, [minesCount]);
 
 
   const resetGame = () => {
     const newBoxes = Array(5 * 5).fill("safe")
-
-    for (let index = 0; index < minesCount; index++) {
+    let index = 0
+    while (index < minesCount) {
       const random = Math.floor(Math.random() * newBoxes.length)
       if (newBoxes[random] !== "mines") {
         newBoxes[random] = "mines"
-
-
-
-
+        index++
       }
     }
+
     setMaindisable(true)
-     dispatch(togglemain(true))
+    dispatch(togglemain(true))
     setBoxes(newBoxes)
     setRevealed(Array(5 * 5).fill(false))
   }
 
 
   const handleBoxclick = (index) => {
+    if(footerselector){
 
     const newRevealed = [...revealed]
     newRevealed[index] = "true"
     setRevealed(newRevealed)
-    console.log("clicked", index)
 
     if (boxes[index] == "mines") {
       alert("you lose 💣💥")
@@ -57,30 +56,43 @@ const Main = () => {
       }, 1500);
 
     }
+  }
+  }
+
+  const togglemineselector = () =>{
+   setTogglemine(!togglemine)
 
   }
-  console.log(boxes, revealed)
+
+  const changemines= (mines)=>{
+    setMinesCount(mines)
+    setTogglemine(true)
+  
+  }
 
 
   return (
-    <main className={`flex flex-col justify-between items-center h-8/10 ${maindisable ? "disable-main" : ""} `}>
-      <div className='flex flex-col md:w-1/3 w-9/10 mt-2'>
+    <main className={`flex flex-col justify-between items-center h-8/10  `}>
+      <div className={`flex flex-col md:w-1/3 w-9/10 mt-2 ${footerselector ? "disable-main" : ""}`}>
 
         <div className=' bg-blue-900 h-6 rounded-2xl flex justify-between items-center'>
-          <div className='flex items-center h-5 mx-px'>
-            <select name="Mines" className='bg-blue-700  rounded-xl  m-px text-white' >
-              <option selected value="1">Mines 1</option>
-              <option className='bg-red-200' value="2">Mines 2</option>
-              <option value="3">Mines 3</option>
-              <option value="4">Mines 4</option>
-              <option value="5">Mines 5</option>
-            </select>
+          <div className='flex items-center h-5 mx-px '>
+            <div onClick={togglemineselector} className='bg-sky-700 cursor-pointer rounded-xl px-8 h-5 text-white flex justify-center items-center' >
+              Mines : {minesCount}
+            </div>
+            <div className={`h-55 w-43 bg-slate-800 fixed md:top-17 top-9 overflow-y-scroll rounded-xl ${togglemine?"hidden":""}`}>
+             {totalmines.map((mines)=>{
+             return <div onClick={()=>changemines(mines)} className={` cursor-pointer text-white mx-4 mt-4 mb-4 h-5 flex justify-center items-center rounded-xl ${(minesCount)==mines?"bg-sky-700":"bg-sky-900"}`}>{mines}</div>
+             })}
+             
+            
+            </div>
           </div>
-          <div className='bg-orange-400 rounded-xl px-1 mx-px text-sm text-white h-5'>Next: 1.10X</div>
+          <div className='bg-orange-400 rounded-xl px-8 mx-px text-sm text-white h-5'>Next: 1.10X</div>
         </div>
         <div className=' bg-blue-900 h-1 my-2 rounded-2xl flex justify-between items-center'></div>
       </div>
-      <div className={`md:w-1/3 md:h-86  h-1/3 w-8/10 bg-blue-600 rounded-2xl flex flex-wrap justify-between md:gap-x-2 gap-x-1 items-center`}>
+      <div className={`md:w-1/3 md:h-86  h-1/3 w-8/10 bg-blue-600 rounded-2xl flex flex-wrap justify-between md:gap-x-2 gap-x-1 items-center ${mainselector ? "disable-main" : ""}`}>
         {boxes.map((box, index) => {
           return <div key={index} onClick={() => handleBoxclick(index)} className={`h-1/6 md:text-3xl text-xl cursor-pointer w-1/6 md:rounded-xl rounded-md border-4  flex justify-center items-center
           ${revealed[index]
@@ -89,7 +101,7 @@ const Main = () => {
               : "grad"
             }
           `}>
-            {revealed[index] ? box == "safe" ? "🌟" : "💣" : "🔵"}
+            {revealed[index] ? box == "safe" ? "🌟" : "💣" :  <div className={`h-5 w-5 rounded-full gradcircle `}></div> }
 
             {/* <div className={`h-5 w-5 rounded-full gradcircle `}></div> */}
           </div>
