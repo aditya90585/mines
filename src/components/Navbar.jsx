929f6a40bcc4752b0c739f124edfe167bbdfa991
Aditya Rajput <adityarajput81971@gmail.com>
2025-07-01 11:50:41 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
 
-    <nav className='md:bg-blue-800  w-[99%] h-8 rounded-2xl flex justify-between items-center md:static absolute bottom-2'>
+    <nav className='md:bg-blue-800  w-[99%] h-8 rounded-2xl flex justify-between items-center md:static absolute bottom-0'>
       <div className='flex w-1/3'>
         <div className='h-6 ml-1  w-35 p-1 text-white bg-blue-500 rounded-xl flex items-center justify-center'>Mines</div>
         <div className='h-6 ml-4 md:w-35 w-8 p-1 bg-orange-400 rounded-xl flex items-center justify-center'><span className='md:flex hidden'>How To Play</span> ?</div>
